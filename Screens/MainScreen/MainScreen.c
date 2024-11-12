37c7f8211009e21fcc9d8b26115d9ce2c73ecac0
Kai <imatch181@gmail.com>
2024-11-12 15:55:17 +0800

    Complete drawer animation.

--- Screens/MainScreen/MainScreen.c
+++ Screens/MainScreen/MainScreen.c
@@ -11,6 +11,7 @@ void MainScreen(GtkBuilder *builder) {
     GtkWidget *drawerIconImage = gtk_image_new_from_file(ROOT_PATH"/src/Icons/menu.png");
     gtk_image_set_icon_size((GtkImage *) drawerIconImage, 100);
     gtk_box_append(drawerIconBox, drawerIconImage);
+
     // 取得按鈕
     GtkWidget *button = (GtkWidget *) gtk_builder_get_object(builder, "DrawerButton");
     GtkWidget *drawerBox = (GtkWidget *) gtk_builder_get_object(builder, "DrawerBox");
@@ -21,38 +22,40 @@ void MainScreen(GtkBuilder *builder) {
 bool isDrawerOpen = false;
 bool isAnimating = false;
 
-void *drawerAnimation(gpointer data);
+gboolean drawerAnimation(gpointer data);
 
 void DrawerButtonOnClick(GtkWidget *button, gpointer data) {
-    if(isAnimating) return;
-    pthread_t animationThread;
-    pthread_create(&animationThread,NULL, drawerAnimation, data);
+    if (isAnimating) return;
+    isAnimating = true;
+    g_idle_add(drawerAnimation, data);
 }
 
-void *drawerAnimation(gpointer data) {
-    struct timespec ts;
-    ts.tv_sec = 0;            // 秒
-    ts.tv_nsec = 0.5 ms;     // 納秒 (1毫秒)
-
-    isAnimating = true;
+gboolean drawerAnimation(gpointer data) {
+    static int step = 0;
+    static int dWidth = 10;
     GtkWidget *drawer = (GtkWidget *) data;
-    int lWidth = 0; //上次的寬度
-    int dWidth = 10; //每次增加的寬度
-    int addCount = 30; //增加的次數
-    dWidth *= isDrawerOpen ? -1 : 1;
-    for (int i = 0; i < addCount; i++) {
-        int width = gtk_widget_get_width(drawer);
-        width += dWidth;
-        while (lWidth == width) {
-            width = gtk_widget_get_width(drawer);
-            width += dWidth;
-            nanosleep(&ts, NULL);
-        }
-        printf("%d\n", width);
-        lWidth = width;
-        gtk_widget_set_size_request(data, width, -1);
+
+    if (step == 0) {
+        dWidth = 10 * (isDrawerOpen ? -1 : 1);
     }
-    isDrawerOpen = !isDrawerOpen;
-    isAnimating = false;
-    pthread_exit(NULL);
+    step++;
+
+    int width = gtk_widget_get_width(drawer);
+    width += dWidth;
+    if (width > 300) {
+        width = 300;
+        step = 0;
+    }
+    if (width < 65) {
+        width = 65;
+        step = 0;
+    }
+    printf("%d\n", width);
+    gtk_widget_set_size_request(drawer, width, -1);
+    if (step == 0) {
+        isDrawerOpen = !isDrawerOpen;
+        isAnimating = false;
+        return G_SOURCE_REMOVE; // 停止動畫
+    }
+    return G_SOURCE_CONTINUE; // 繼續動畫
 }
