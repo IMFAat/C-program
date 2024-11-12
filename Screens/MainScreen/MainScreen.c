12e82ca640211bcb25eaf46e8a2bb36ad919d672
Kai <imatch181@gmail.com>
2024-11-12 09:14:30 +0800

    Complete drawer animation.

--- Screens/MainScreen/MainScreen.c
+++ Screens/MainScreen/MainScreen.c
@@ -1,6 +1,9 @@
 #include <gtk/gtk.h>
+#include <pthread.h>
+#define ms *1000
 
 void DrawerButtonOnClick(GtkWidget *button, gpointer data);
+
 //主畫面
 void MainScreen(GtkBuilder *builder) {
     // 取得 DrawerIconBox 並加上圖示
@@ -8,7 +11,6 @@ void MainScreen(GtkBuilder *builder) {
     GtkWidget *drawerIconImage = gtk_image_new_from_file(ROOT_PATH"/src/Icons/menu.png");
     gtk_image_set_icon_size((GtkImage *) drawerIconImage, 100);
     gtk_box_append(drawerIconBox, drawerIconImage);
-
     // 取得按鈕
     GtkWidget *button = (GtkWidget *) gtk_builder_get_object(builder, "DrawerButton");
     GtkWidget *drawerBox = (GtkWidget *) gtk_builder_get_object(builder, "DrawerBox");
@@ -16,5 +18,37 @@ void MainScreen(GtkBuilder *builder) {
     g_signal_connect(button, "clicked", G_CALLBACK(DrawerButtonOnClick), drawerBox);
 }
 
+bool isDrawerOpen = false;
+bool isAnimating = false;
+
+void *drawerAnimation(gpointer data);
+
 void DrawerButtonOnClick(GtkWidget *button, gpointer data) {
+    if(isAnimating) return;
+    pthread_t animationThread;
+    pthread_create(&animationThread,NULL, drawerAnimation, data);
+}
+
+void *drawerAnimation(gpointer data) {
+    isAnimating = true;
+    GtkWidget *drawer = (GtkWidget *) data;
+    int lWidth = 0; //上次的寬度
+    int dWidth = 10; //每次增加的寬度
+    int addCount = 30; //增加的次數
+    dWidth *= isDrawerOpen ? -1 : 1;
+    for (int i = 0; i < addCount; i++) {
+        int width = gtk_widget_get_width(drawer);
+        width += dWidth;
+        while (lWidth == width) {
+            width = gtk_widget_get_width(drawer);
+            width += dWidth;
+            usleep(0.5 ms);
+        }
+        printf("%d\n", width);
+        lWidth = width;
+        gtk_widget_set_size_request(data, width, -1);
+    }
+    isDrawerOpen = !isDrawerOpen;
+    isAnimating = false;
+    pthread_exit(NULL);
 }
